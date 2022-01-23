f58690e07dfe57a9ab5a428198da708accbb842e
Dalton Ma <85321730+dalton77-get@users.noreply.github.com>
2022-01-22 19:13:35 -0600

    cherrypy

---
+++ highlight/server.py
@@ -0,0 +1,50 @@
+# Import your application as:
+# from wsgi import application
+# Example:
+
+from highlight.wsgi import application
+from sys import argv, exit
+
+# Import CherryPy
+import cherrypy
+
+if __name__ == '__main__':
+    if not sys.argv[1]:
+        exit(1)
+    # Mount the application
+    cherrypy.tree.graft(application, "/")
+
+    # Unsubscribe the default server
+    cherrypy.server.unsubscribe()
+
+    # Instantiate a new server object
+    server = cherrypy._cpserver.Server()
+    port = int(sys.argv[1])
+    # Configure the server object
+    server.socket_host = "0.0.0.0"
+    server.socket_port = port
+    server.thread_pool = 30
+
+    # For SSL Support
+    # server.ssl_module            = 'pyopenssl'
+    # server.ssl_certificate       = 'ssl/certificate.crt'
+    # server.ssl_private_key       = 'ssl/private.key'
+    # server.ssl_certificate_chain = 'ssl/bundle.crt'
+
+    # Subscribe this server
+    server.subscribe()
+
+    # Example for a 2nd server (same steps as above):
+    # Remember to use a different port
+
+    # server2             = cherrypy._cpserver.Server()
+
+    # server2.socket_host = "0.0.0.0"
+    # server2.socket_port = 8081
+    # server2.thread_pool = 30
+    # server2.subscribe()
+
+    # Start the server engine (Option 1 *and* 2)
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
